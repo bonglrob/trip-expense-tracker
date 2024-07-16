06a7a8d3120c7a7f765a1c5baf3ccac69b8ac215
Robert Bonglamphone <rob.bonglamphone@gmail.com>
2024-07-15 22:07:11 -0700

    remove duplicate empty expenses import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import EmptyExpenses from './components/EmptyExpenses';
 import EmptyBalances from './components/EmptyBalances';
 import FilledBalances from './components/FilledBalances';
 import Stats from './components/Stats';
-import EmptyExpenses from './components/EmptyExpenses.js';
 import CreateExpense from './components/CreateExpense.js';
 import FilterExpenses from './components/FilterExpenses.js';
 
