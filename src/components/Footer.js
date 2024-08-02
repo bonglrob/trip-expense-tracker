fc3420cb8aed8175c2e5155f37a9303b67ca2890
emma-esteboss <esteb@uw.edu>
2024-08-01 21:06:37 -0700

    meeting 8/1 changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,9 +14,11 @@ export default function Footer(props) {
                 </div>
                 <p>&copy; TripSplit <a href="https://github.com/info340-su24/travel-expenses-B7"><img className="github" src="image/github-icon.svg" alt="github icon" /></a></p>
                 <p className="text-center">
-                    Created by Robert Bonglamphone, Emma Esteban, and Nick Chiu<br />
-                    Part of University of Washington course INFO 340
+                    Created by Robert Bonglamphone, Emma Esteban, and Nick Chiu
                 </p>            
+                <p className="text-center">
+                    Part of University of Washington course INFO 340
+                </p> 
             </div>
         </footer>
     ); 
