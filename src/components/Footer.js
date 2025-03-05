152d5c40a2a32583364e3876a759cf8d810b0dff
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-03-05 13:19:39 -0800

    remove footer attribution and change github icon to white
    Attribution is removed because all contributions can still be traced
    back to the source code. Extra footer text is too distracting to the
    user, so removal can provide a more clean look.
    
    Github icon changed to white to meet higher color contrast ratio of at
    least 3:1 for readability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,12 @@
-import { Link } from 'react-router-dom';
-
 export default function Footer(props) { 
     
     return (
-        <footer className="mt-auto">
-            <div className="d-flex flex-column align-items-center">
-                <p>&copy; TripSplit <a href="https://github.com/info340-su24/travel-expenses-B7"><img className="github" src="image/github-icon.svg" alt="github icon" /></a></p>
-                <p className="text-center">
-                    Created by Robert Bonglamphone, Emma Esteban, and Nick Chiu
-                </p>            
-                <p className="text-center">
-                    Part of University of Washington course INFO 340
-                </p> 
+        <footer className="d-flex justify-content-center mt-auto">
+            <div className="d-flex gap-2 align-items-center">
+                <p className="m-1">&copy; 2025 TripSplit </p>
+                <a href="https://github.com/bonglrob/trip-expense-tracker">
+                    <img className="github" src="image/github-icon.svg" alt="github icon" />
+                </a>       
             </div>
         </footer>
     ); 
